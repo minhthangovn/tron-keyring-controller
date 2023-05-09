858508ef4ed9f5a084e84b8a119a0d840ec7fa19
Ly Minh <minduc2020@gmail.com>
2023-05-09 12:04:03 +0700

    upgrade bip32

--- tron-hd-keyring.js
+++ tron-hd-keyring.js
@@ -9,7 +9,12 @@
 
 const log = require('loglevel')
 const EventEmitter = require('events').EventEmitter
-const bip32 = require('bip32')
+// const bip32 = require('bip32')
+const ecc = require('tiny-secp256k1')
+const { BIP32Factory } = require('bip32')
+// You must wrap a tiny-secp256k1 compatible implementation
+const bip32 = BIP32Factory(ecc)
+
 const bip39 = require('bip39')
 const TronWallet = require('./tron-wallet')
 
